36a8d1c12630f273637194d557778435b3deece5
Yury Voloshin <yvoloshin01@gmail.com>
2016-05-09 05:46:37 +0000

    Styling changes

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,12 @@ module StrengthTracker
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Add the fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
+    
   end
 end
