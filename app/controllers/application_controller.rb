2c7f26ac6279986af5ea4026c4c8fa4e568c99f9
Yury Voloshin <yvoloshin01@gmail.com>
2016-07-10 15:33:33 +0000

    Add username to Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,37 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+ #  def configure_permitted_parameters
+ #   devise_parameter_sanitizer.for(:sign_in)        << :username
+ #   devise_parameter_sanitizer.for(:sign_up)        << :username
+ #   devise_parameter_sanitizer.for(:account_update) << :username
+	# end
+
+	 def configure_permitted_parameters
+    registration_params = [:email, :usernamename, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(registration_params) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(registration_params << :current_password) }
+  end
+
+	# def configure_permitted_parameters
+	#   devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+	#     user_params.permit(:username, :email)
+	#   end
+		
+
+	# 	devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+	#     user_params.permit(:username, :email)
+	#   end
+		
+
+	# 	devise_parameter_sanitizer.permit(:account_update) do |user_params|
+	#     user_params.permit(:username, :email)
+	#   end
+	# end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,40 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
   has_many :workouts
   has_many :workout_types
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  attr_accessor :login
+  validates :username,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  }
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_hash).first
+    end
+  end
+
+ #  validate :validate_username
+
+	# def validate_username
+	#   if User.where(email: username).exists?
+	#     errors.add(:username, :invalid)
+	#   end
+	# end
 
 end
