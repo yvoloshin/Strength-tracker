319cfa009b30648284e4d516fe928a6bea1c2dbd
Yury Voloshin <yvoloshin01@gmail.com>
2016-09-24 03:47:48 +0000

    Change additional mailing parameters: emails are now sent from localhost

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,21 +1,21 @@
-Rails.application.configure do
+StrengthTracker::Application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost:3030' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.action_mailer.raise_delivery_errors = true
 
-  # config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
 
-  # config.action_mailer.smtp_settings = {
-  # address: "smtp.gmail.com",
-  # port: 587,
-  # domain: ENV["GMAIL_DOMAIN"],
-  # authentication: "plain",
-  # enable_starttls_auto: true,
-  # user_name: ENV["GMAIL_USERNAME"], #try wrong password
-  # password: ENV["GMAIL_PASSWORD"]
-  # }
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: 'example.com',
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"], #try wrong password
+  password: ENV["GMAIL_PASSWORD"]
+  }
 
   config.action_mailer.default :charset => "utf-8"
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ ActionMailer::Base.smtp_settings = {
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
+    :domain         => 'strength-tracker.heroku.com',
     :authentication => :plain,
   }
 
