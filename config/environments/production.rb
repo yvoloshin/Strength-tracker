0c5030a77f83d9972975fd57d87d6f4be575694e
Yury Voloshin <yvoloshin01@gmail.com>
2016-10-01 04:24:03 +0000

    Add sendgrid to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,5 @@
-Rails.application.configure do
+StrengthTracker::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
-  Rails.application.routes.default_url_options[:host] = 'strength-tracker.herokuapp.com'
 
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.perform_deliveries = true
@@ -19,13 +17,14 @@ Rails.application.configure do
 # }
 
 ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'strength-tracker.heroku.com',
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
     :authentication => :plain,
-  }
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+    }
 
   ActionMailer::Base.delivery_method = :smtp
 
