9449574f36a3308a758d7f680f0044e008314d62
Yury Voloshin <yvoloshin01@gmail.com>
2016-10-06 04:36:05 +0000

    Add logo and adjust layout

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,6 +1,6 @@
 class Workout < ActiveRecord::Base
 	belongs_to :workout_type
-	has_many :exercises
+	has_many :exercises, dependent: :destroy
 	belongs_to :user
 
 	accepts_nested_attributes_for :exercises
