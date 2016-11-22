ecda4fdcbc1746040c19753e2fde6e3e27096dc8
Yury Voloshin <yvoloshin01@gmail.com>
2016-11-22 05:07:56 +0000

    Add FAQ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -17,7 +18,7 @@ Rails.application.routes.draw do
     resources :workouts, :only => [:index, :show, :delete]
   end
 
-  get 'faq', :to => 'faq'
+  get 'faq', :to => 'static_pages#faq'
 
 
 
