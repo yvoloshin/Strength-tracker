2e678ddd367ffc092af9262e585979a2f8a38270
Yury Voloshin <yvoloshin01@gmail.com>
2017-03-18 06:35:23 +0000

    Adjust login link; change load to integer

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304053640) do
+ActiveRecord::Schema.define(version: 20170318062349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "completed_sets", force: :cascade do |t|
     t.integer  "reps"
-    t.string   "load"
     t.integer  "exercise_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "load"
   end
 
   create_table "exercise_types", force: :cascade do |t|
