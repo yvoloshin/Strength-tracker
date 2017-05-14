bdd8906541b1d8c1fb41504434d16368ec91e578
Yury Voloshin <yvoloshin01@gmail.com>
2017-05-14 06:55:35 +0000

    Fix WorkoutTypes update method; add links to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@
 @bike_workout_type = WorkoutType.create({
 	user_id: 1, 
 	type_name: "Bike Muscular Enhancement", 
-	description: "This workout is from 'Weight Training for Triathlon' by Ben Greenfield. 
+	description: "This workout is from <a href='https://read.amazon.com/kp/embed?asin=B006986EWA&preview=newtab&linkCode=kpe&ref_=cm_sw_r_kb_dp_vV-fzb33HE8HZ&tag=routinetrack-20'>Weight Training for Triathlon</a> by Ben Greenfield. 
 	It is designed to strengthen cycling-specific muscles. Prior to this workout, it is recommended to do 3-5 minutes of 
 	aerobic exercise, followed by dynamic stretching and foam rolling. The exercises should be done in a sequence,
 	one after another with 10-20 seconds of rest in between. A sequence consists of one set of each exercise.
@@ -19,7 +19,7 @@
 	The load is specified as the percentage 
 	of the maximum weight you can lift in one rep. Testing your one rep max directly is too risky and is never
 	recommended, but there are a number of easy formulas and online calculators that can be used to estimate it.
-	For example, you can use the calculator at http://www.bodybuilding.com/fun/other7.htm",
+	For example, you can use the calculator at <a href='http://www.bodybuilding.com/fun/other7.htm'>http://www.bodybuilding.com/fun/other7.htm</a>",
 	public: true,
 	is_visible: true
 })
@@ -51,14 +51,14 @@ end
 @run_workout_type = WorkoutType.create({
 	user_id: 1, 
 	type_name: "Running Foundational Strength", 
-	description: "This workout is from 'Weight Training for Triathlon' by Ben Greenfield. 
+	description: "This workout is from <a href='https://www.amazon.com/dp/B006986EWA/ref=cm_sw_r_tw_dp_x_vV-fzb33HE8HZ'>Weight Training for Triathlon</a> by Ben Greenfield.  
 	It is designed to strengthen running-specific muscles. Prior to this workout, it is recommended to do 3-5 minutes of 
 	aerobic exercise, followed by dynamic stretching and foam rolling. The exercises should be done in a sequence,
 	one after another with 10-20 seconds of rest in between. A sequence consists of one set of each exercise.
 	Repeat the sequence for as many sets as specified in the routine. Descriptions of each exercise can be easily found on Youtube. The load is specified as the percentage 
 	of the maximum weight you can lift in one rep. Testing your one rep max directly is too risky and is never
 	recommended, but there are a number of easy formulas and online calculators that can be used to estimate it.
-	For example, you can use the calculator at http://www.bodybuilding.com/fun/other7.htm",
+	For example, you can use the calculator at <a href='http://www.bodybuilding.com/fun/other7.htm'>http://www.bodybuilding.com/fun/other7.htm</a>",
 	public: true,
 	is_visible: true
 })
@@ -89,7 +89,7 @@ end
 @ten_to_one_workout_type = WorkoutType.create({
 	user_id: 1, 
 	type_name: "10-1 Full Body Workout", 
-	description: "This workout is from Jen Eddins at peanutbutterrunner.com. The exercises in each set are meant to be done as a circuit. Start with 10 reps of each exercise in a set and work down to 1. The original workout had front squats, which I replaced with one-legged squats here.",
+	description: "This workout is from Jen Eddins at <a href='http://www.peanutbutterrunner.com'>peanutbutterrunner.com</a>. The exercises in each set are meant to be done as a circuit. Start with 10 reps of each exercise in a set and work down to 1. The original workout had front squats, which I replaced with one-legged squats here.",
 	public: true,
 	is_visible: true
 })
