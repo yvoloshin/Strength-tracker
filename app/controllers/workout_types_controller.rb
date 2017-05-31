912cbd3671744bd33c8dc62d6824842fe67dbb09
Yury Voloshin <yvoloshin01@gmail.com>
2017-05-31 03:57:58 +0000

    Add url to exercise_type

--- app/controllers/workout_types_controller.rb
+++ app/controllers/workout_types_controller.rb
@@ -71,7 +71,7 @@ class WorkoutTypesController < ApplicationController
   end
 
 	def workout_type_params
-		params.require(:workout_type).permit(:type_name, :public, :description, exercise_types_attributes: [:id, :name, :sets, :reps, :load])
+		params.require(:workout_type).permit(:type_name, :public, :description, exercise_types_attributes: [:id, :name, :sets, :reps, :load, :url])
 	end
 
 	def exercise_params
