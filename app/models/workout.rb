1ed6a00e6d9d680a6792993a0c29f8d959296062
Yury Voloshin <yvoloshin01@gmail.com>
2019-01-23 04:39:20 +0000

    Extract counting sets into a function; add function comments

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -45,14 +45,17 @@ class Workout < ActiveRecord::Base
 	  end
 	end
 
+	# Returns a hash containing comparisons between the number of sets,
+	# the total weight lifted, or the total reps (for bodyweight exercises)
+	# for the same exercise between two recorded workouts belonging to the same routine
 	def compare_with_previous(workout, previous_workout)
 		comparisons = Hash.new
 
 		workout.exercises.each do |exercise|
 			comparisons[exercise.name] = Hash.new
 
-			current_sets = Workout.joins(exercises: :completed_sets).where(exercises: {name: exercise.name}, id: workout.id).count
-			previous_sets = Workout.joins(exercises: :completed_sets).where(exercises: {name: exercise.name}, id: previous_workout.id).count
+			current_sets = get_total_sets_count(workout, exercise.name)
+			previous_sets = get_total_sets_count(previous_workout, exercise.name)
 			difference_sets = current_sets - previous_sets
 
 			if difference_sets == 0
@@ -63,8 +66,8 @@ class Workout < ActiveRecord::Base
 				comparisons[exercise.name][:sets_message] = "#{difference_sets} fewer sets this time"				
 			end	
 
-			current_load = CompletedSet.joins(:exercise).where(exercises: {name: exercise.name, workout_id: workout.id}).select("load").order("exercises.created_at").as_json
-			current_reps = CompletedSet.joins(:exercise).where(exercises: {name: exercise.name, workout_id: workout.id}).select("reps").order("exercises.created_at").as_json
+			current_load = CompletedSet.joins(:exercise).where(exercises: {name: exercise.name, workout_id: workout.id}).order("exercises.created_at").as_json
+			current_reps = CompletedSet.joins(:exercise).where(exercises: {name: exercise.name, workout_id: workout.id}).order("exercises.created_at").as_json
 
 			current_total_load = 0
 
@@ -76,7 +79,13 @@ class Workout < ActiveRecord::Base
 
 			previous_load = CompletedSet.joins(:exercise).where(exercises: {name: exercise.name, workout_id: previous_workout.id}).select("load").order("exercises.created_at").as_json
 			previous_reps = CompletedSet.joins(:exercise).where(exercises: {name: exercise.name, workout_id: previous_workout.id}).select("reps").order("exercises.created_at").as_json
-			
+			previous_load_sum = CompletedSet.joins(:exercise).where(exercises: {name: exercise.name, workout_id: previous_workout.id}).select("load").order("exercises.created_at").sum("load")
+
+			body_weight = false
+			if current_load_sum == 0 && previous_load_sum ==0
+				body_weight = true
+			end
+
 			previous_total_load = 0
 
 			previous_load.each_with_index do |item, index|
@@ -98,6 +107,17 @@ class Workout < ActiveRecord::Base
 		end
 
 		return comparisons
+	end
+
+	# Returns the number of sets for an exercise completed in a given workout
+	def get_total_sets_count(workout, exercise_name)
+		Workout.joins(exercises: :completed_sets).where(exercises: {name: exercise_name}, id: workout.id).count
+	end
+
+	# Returns the total weight lifted, or the total number of reps for a bodyweight exercise,
+	# for an exercise completed in a given workout
+	def get_total_sets_results(workout, exercise_name)
+
 	end
 
 end
