5405c4d927709ab79ec0f134d1ba84c7267a8482
Yury Voloshin <yvoloshin01@gmail.com>
2019-02-24 22:02:28 +0000

    Add units to db tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170531033338) do
+ActiveRecord::Schema.define(version: 20190224215203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170531033338) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "load"
+    t.string   "units"
   end
 
   create_table "exercise_types", force: :cascade do |t|
@@ -33,6 +34,7 @@ ActiveRecord::Schema.define(version: 20170531033338) do
     t.datetime "updated_at",      null: false
     t.string   "load"
     t.string   "url"
+    t.string   "units"
   end
 
   add_index "exercise_types", ["workout_type_id"], name: "index_exercise_types_on_workout_type_id", using: :btree
